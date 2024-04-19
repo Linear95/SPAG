9e6dc0c18fcc6a4e7af7ba11e78fccbfe401ad62
Pengyu Cheng <30513356+Linear95@users.noreply.github.com>
2024-04-19 16:57:44 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -28,9 +28,9 @@ DEFAULT_UNK_TOKEN = "<unk>"
 GAME_RULE_PROMPTS=[
 '''Play the game of Adversarial Taboo. In this game, there are two players, an attacker and a defender.
 
-At the beginning, attacker is assigned with a target word, with which the defender is not informed. The task of the attacker is to induce the defender to utter the target word unconciously, then the attacker win the game. However, the attacker is not allow to utter the target word, otherwise the attacker loses.
+At the beginning, the attacker is assigned a target word, with which the defender is not informed. The task of the attacker is to induce the defender to utter the target word unconciously, then the attacker win the game. However, the attacker is not allow to utter the target word, otherwise the attacker loses.
 
-At the same time, the defender tries to figure out the target word. if the defender indentifies the target word, he can speak "I know the word! It is `target word`!". Then the defender wins. Remember, the defender can only guess the word once, otherwise he will directly lose the game.
+At the same time, the defender tries to figure out the target word. If the defender identifies the target word, he can say "I know the word! It is `target word`!". Then the defender wins. Remember, the defender can only guess the word once, otherwise he will directly lose the game.
 
 The game has {max_turns} turns. if nobody wins during {max_turns} turns, there is a tie.
 ''',
